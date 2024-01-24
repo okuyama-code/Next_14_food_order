996a8daef67fec4c9690386af67d0ca98b0c76fb
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2024-01-25 07:59:35 +0900

    修正

--- src/app/components/layout/Header.jsx
+++ src/app/components/layout/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header>
-      <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+    <header className='flex items-center justify-between'>
+      <nav className="flex items-center text-gray-500 font-semibold gap-8">
         <Link className="text-primary font-semibold text-2xl" href={'/'}>
           ST PIZZA
         </Link>
@@ -12,7 +12,11 @@ const Header = () => {
         <Link href={'/menu'}>Menu</Link>
         <Link href={'/#about'}>About</Link>
         <Link href={'/#contact'}>Contact</Link>
-        <Link href={'/'} className="bg-primary rounded-full text-white px-8 py-2">Login</Link>
+      </nav>
+
+      <nav className='flex items-center gap-4 text-gray-400'>
+        <Link href={'/login'} className="bg-primary rounded-full text-white px-8 py-2">Login</Link>
+        <Link href={'/register'} className="bg-primary rounded-full text-white px-8 py-2">Register</Link>
       </nav>
     </header>
   )
