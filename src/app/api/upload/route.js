ae2eff2c6abc310ae65be483addfbc05bd408b20
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2024-01-26 19:01:48 +0900

    修正

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,5 +1,7 @@
 import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
 import uniqid from 'uniqid';
+import { format } from 'date-fns' // 追加
+
 
 export async function POST(req) {
   const data =  await req.formData();
@@ -8,7 +10,7 @@ export async function POST(req) {
     const file = data.get('file');
 
     const s3Client = new S3Client({
-      region: 'us-east-1',
+      region: 'ap-northeast-1',
       credentials: {
         accessKeyId: process.env.MY_AWS_ACCESS_KEY,
         secretAccessKey: process.env.MY_AWS_SECRET_KEY,
@@ -17,7 +19,6 @@ export async function POST(req) {
 
     const ext = file.name.split('.').slice(-1)[0];
     const newFileName = uniqid() + '.' + ext;
-    console.log(newFileName)
 
     const chunks = [];
     for await (const chunk of file.stream()) {
@@ -25,13 +26,21 @@ export async function POST(req) {
     }
     const buffer = Buffer.concat(chunks);
 
-    const bucket = 'okuyama-food-ordering';
+    const bucket = 'okuyama1-food-ordering';
+
+    // 現地時間を取得し、それをUTCから日本時間に変換
+    const japanTime = format(new Date(), 'yyyy-MM-dd HH:mm:ss', { timeZone: 'Asia/Tokyo' });
+
     await s3Client.send(new PutObjectCommand({
       Bucket: bucket,
       Key: newFileName,
       ACL: 'public-read',
       ContentType: file.type,
       Body: buffer,
+      Metadata: {
+        // 現地時間をファイルのメタデータとして追加
+        'japan-time': japanTime,
+      },
     }));
 
 
